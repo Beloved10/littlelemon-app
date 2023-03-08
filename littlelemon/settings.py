3445557a2628da759acbfa69231fc73ecddef361
Ikhide Augustine <augustineimoleme@gmail.com>
2023-03-08 20:29:45 +0100

    My first modification

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     "restaurant/static",
 ]
 
+import os
+ 
+STATIC_ROOT = os.path.join(BASE_DIR, "restaurant/static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
